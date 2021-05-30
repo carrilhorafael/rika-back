1867a17e7fef2de6cd0a6db622271285b9285454
Carrilho <rafael.carrilho@injunior.com.br>
2021-05-30 14:32:27 -0300

    corrção da data do trabalho de casa

--- app/models/homework.rb
+++ app/models/homework.rb
@@ -6,7 +6,7 @@ class Homework < ApplicationRecord
     validate :deadline_valid, on: :create
   ## funções
     def deadline_valid
-      errors.add :deadline, "Você só pode marcar a entrega da tarefa após a data da aula" if deadline < lesson.schedule
+      errors.add :deadline, "Você só pode marcar a entrega da tarefa após a data da aula" if deadline < Date.today
     end
   
 end
