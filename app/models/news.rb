dee07b470606ed6c7545d5d816f6117fb1ede36f
Carrilho <rafael.carrilho@injunior.com.br>
2021-05-30 12:34:28 -0300

    responsável pela matéria não pode ser estudante

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,11 +1,5 @@
 class News < ApplicationRecord
   belongs_to :user
   validates :title, :content, presence: true
-  validate :author_must_be_admin
-
-  def author_must_be_admin
-    unless user.administrador?
-      errors.add :unauthorized, "Você não tem permissão para criar uma notícia"
-    end  
-  end
+  validate :author_must_be_admin  
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,7 +1,15 @@
 class Subject < ApplicationRecord
     ## relações
         has_many :lessons
+    
     ## validações
         validates :name, presence: true
+        validate :user_cannot_be_student
     ## funções
+        def user_cannot_be_student
+            if user.student?
+                errors.add :is_student, "Esse usuário não pode dar aula"
+            end
+        end
+
 end
