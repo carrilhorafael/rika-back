12570c41e3c106b41381d1f50181e3b6e7c18a69
Carrilho <rafael.carrilho@injunior.com.br>
2021-05-30 12:42:16 -0300

    comentario das partes de seed q bugam em producao

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-administrador = User.create(name: "Rafael Carrilho", email: "rafael.carrilho@injunior.com.br", password:"12345678", password_confirmation:"12345678", role:1)
-estudante = User.create(name: "Thiago Andrade", email: "thiago.andrade@injunior.com.br", password:"12345678", password_confirmation:"12345678", role:3)
-instrutor = User.create(name: "Gyselle Melo", email: "gyselle.melo@injunior.com.br", password:"12345678", password_confirmation:"12345678", role:2)
+# administrador = User.create(name: "Rafael Carrilho", email: "rafael.carrilho@injunior.com.br", password:"12345678", password_confirmation:"12345678", role:1)
+# estudante = User.create(name: "Thiago Andrade", email: "thiago.andrade@injunior.com.br", password:"12345678", password_confirmation:"12345678", role:3)
+# instrutor = User.create(name: "Gyselle Melo", email: "gyselle.melo@injunior.com.br", password:"12345678", password_confirmation:"12345678", role:2)
 
-25.times.each do 
-    News.create(title: "Nova notícia", user_id: 1, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras varius libero at diam egestas, eget interdum nisi dignissim. Curabitur eu porta ante. Sed fringilla erat dui, at lacinia orci suscipit quis. Nulla accumsan nec mauris nec pretium. Phasellus molestie convallis cursus. Morbi nisl enim, suscipit hendrerit bibendum nec, fermentum id sem. Donec varius fringilla turpis. Nam eleifend dictum erat, in egestas dui tincidunt a. Duis vitae aliquet eros.")
-end
+# 25.times.each do 
+#     News.create(title: "Nova notícia", user_id: 1, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras varius libero at diam egestas, eget interdum nisi dignissim. Curabitur eu porta ante. Sed fringilla erat dui, at lacinia orci suscipit quis. Nulla accumsan nec mauris nec pretium. Phasellus molestie convallis cursus. Morbi nisl enim, suscipit hendrerit bibendum nec, fermentum id sem. Donec varius fringilla turpis. Nam eleifend dictum erat, in egestas dui tincidunt a. Duis vitae aliquet eros.")
+# end
 
 subjects = ["Rails", "React", "HTML", "CSS", "Javascript", "Ruby", "Criação"]
 subjects.each do |sub|
